96db7391b621bd430d1625453d62e54137b4e9cc
Lux <negilux687@gmail.com>
2026-07-31 01:47:39 +0530

    Add files via upload

---
+++ EmailSpamAPI/main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+
+app = FastAPI(title="Spam Detection API")
+
+# Load model
+with open("spam_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+# Load vectorizer
+with open("vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
+
+
+class MailRequest(BaseModel):
+    message: str
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Spam Detection API is Running"
+    }
+
+
+@app.post("/predict")
+def predict(data: MailRequest):
+
+    text = [data.message]
+
+    features = vectorizer.transform(text)
+
+    prediction = model.predict(features)[0]
+
+    if prediction == 1:
+        result = "Spam"
+    else:
+        result = "Ham"
+
+    return {
+        "input": data.message,
+        "prediction": result,
+        "value": int(prediction)
+    }
